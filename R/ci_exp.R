1084380419b2813757fd979c8bbca5e00f37c0de
Gilberto Pereira Sassi <gpsassi@live.com>
2021-04-18 20:29:44 -0300

    confidence interval for exponential distribution. I have to test it.

--- R/ci_exp.R
+++ R/ci_exp.R
@@ -11,4 +11,30 @@
 #' @examples 
 #' x <- rexp(1000)
 #' ci_exp(x)
+ci_exp <- function(x, conf_level = 0.95, type = "two.sided", na.rm = F) {
+  if (!(type %in% c("two.sided", "left", "right"))) {
+    stop("'type' must be one of 'two.sided', 'left' or 'right'.")
+  }
 
+  if (conf_level < 0 | conf_level > 1) {
+    stop("'conf_level' must be a number between 0 and 1.")
+  }
+
+  if (na.rm == TRUE) {
+    x <- x[!is.na(x)]
+  }
+
+  n <- length(x)
+  if (type == "two.sided") {
+    lower_ci <- 2 * n * mean(x) / qchisq((1 - conf_level) / 2, df = 2 * n)
+    upper_ci <- 2 * n * mean(x) / qchisq((1 + conf_level) / 2, df = 2 * n)
+  } else if (type == "left") {
+    lower_ci <- 2 * n * mean(x) / qchisq(conf_level, df = n - 1)
+    upper_ci <- Inf
+  } else if (type == "right") {
+    lower_ci <- 0
+    upper_ci <- 2 * n * mean(x) / qchisq(1 - conf_level, df = n - 1)
+  }
+
+  tibble::tibble(lower_ci, upper_ci, conf_level)
+}
